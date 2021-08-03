b98b325a9f7e81fd435ca922b92955e769a68ebf
Polina <p.echnaya@gmail.com>
2021-08-03 23:18:38 +0300

    create feature news

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import newsSlice from "./slice/newsSlice";
+
+export const store = configureStore({
+  reducer: {
+    news: newsSlice,
+  },
+  devTools: true,
+})
