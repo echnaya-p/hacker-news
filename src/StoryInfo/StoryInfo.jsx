6ad1aeb60851c28572dbee90921c9e5b7db17e85
Polina <p.echnaya@gmail.com>
2021-09-06 22:55:23 +0300

    added button to main page, update comments, auto update comments, formatted comment's text and time

--- src/StoryInfo/StoryInfo.jsx
+++ src/StoryInfo/StoryInfo.jsx
@@ -1,25 +1,27 @@
 import React, {useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import CommentsList from "./CommentsList";
 
 function StoryInfo(props) {
 
   const {id} = useParams();
   const { storiesById, commentsIds, commentsById, onGetComments, onGetKidsComments } = props;
+  const date = new Date(storiesById?.[id]?.time).toLocaleString("ru");
 
     useEffect(() => {
       onGetComments(storiesById?.[id]?.kids ?? []);
-      // const timerId = setInterval( () => {
-      //   onGetComments(storiesById?.[id]?.kids ?? []);
-      // }, 60000);
-      //
-      // return () => {
-      //   clearInterval(timerId);
-      // };
+      const timerId = setInterval( () => {
+         onGetComments(storiesById?.[id]?.kids ?? []);
+       }, 60000);
+
+      return () => {
+         clearInterval(timerId);
+      };
     }, []);
 
 return (
   <div>
+    <Link to='/'>На главную</Link>
     <h2>
       {storiesById?.[id]?.title}
     </h2>
@@ -29,9 +31,10 @@ return (
     </p>
     <ul>
       <li>Автор: {storiesById?.[id]?.by}</li>
-      <li>Дата: {storiesById?.[id]?.time}</li>
+      <li>Дата публикации: {date}</li>
       <li>Коментариев: {storiesById?.[id]?.descendants}</li>
     </ul>
+    <button onClick={() => onGetComments(storiesById?.[id]?.kids ?? [])}>Обновить</button>
     <CommentsList
       commentsIds={commentsIds}
       commentsById={commentsById}
